8559371eb5735b987ea764337889ba5f742ce05f
behije-nezha <106549147+behije-nezha@users.noreply.github.com>
2022-06-10 14:37:57 +0200

    Add files via upload

---
+++ MyProfile.java
@@ -0,0 +1,9 @@
+public class MyProfile {
+    public static void main(String[] args){
+
+        System.out.println("My name is Behije Nezha");
+        System.out.println("I am 21 years old.");
+        System.out.println("I am from Albania.");
+    }
+}
+
